5853c817b247115f0a5001f30cd14aa90bb6bf1e
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2019-03-02 18:31:33 +0900

    create ok

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@
 
 import axios from 'axios'
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENT = 'CREATE_EVENTS'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -12,3 +13,9 @@ export const readEvents = () => async dispatch => {
 //    dispatchでreeducerに渡す
     // 本来ピュアな感酢でないといけないのだが、処理できるようになる 関数を返せるようになる
 }
+
+export const postEvent = values => async dispatch => {
+    const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
+    dispatch({ type: CREATE_EVENT, response }) 
+}
+
